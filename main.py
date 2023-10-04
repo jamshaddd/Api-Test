5b372b5b8a2eb6b9d68ee72728783f1a8e7d6426
microclouds-india <office@microclouds-india.com>
2023-10-04 11:25:24 +0530

    img

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ app = FastAPI()
 
 @app.get('/')
 def index():
-    return {"status":"200"}
+    return {"status":"200","image":"https://raw.githubusercontent.com/jamshaddd/Api-Test/main/uploads/Screenshot%202023-06-05%20115052.png"}
